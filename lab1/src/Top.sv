a3cad961a834c9242550428d6b182f677a8b4a3a
JeterHwang <45968071+JeterHwang@users.noreply.github.com>
2020-09-26 20:50:14 +0800

    Top.sv which can run on FPGA

--- lab1/src/Top.sv
+++ lab1/src/Top.sv
@@ -12,8 +12,8 @@ parameter S_IDLE = 1'b0;
 parameter S_PROC = 1'b1;
 
 // ===== constants =======
-parameter period_add = 32'd1000;
-parameter tmax = 32'd150000;
+parameter period_add = 32'd1000000;
+parameter tmax = 32'd500000000;
 
 // ===== Output Buffers =====
 logic [3:0] o_random_out_r, o_random_out_w;
@@ -44,8 +44,8 @@ always_comb begin
 			state_w = S_PROC;
 			//o_random_out_w = 4'd15;
 			count_w        = 32'd0;
-			period_w       = 32'd1000;
-			next_time_w    = 32'd1000;
+			period_w       = 32'd1000000;
+			next_time_w    = 32'd1000000;
 			LFSR_w         = i_seed_r;
 			pause_w        = 1'b0;
 		end
@@ -60,7 +60,7 @@ always_comb begin
 	end
 	S_PROC: begin
 		//o_random_out_w = 4'd15;
-		state_w 	 = (count_r > tmax) ? S_IDLE : state_w;
+		state_w 	 = (count_r > tmax) ? S_IDLE : state_r;
 		count_w 	 = count_r;
 		LFSR_w 		 = LFSR_r;
 		i_seed_w 	 = i_seed_r;
@@ -69,19 +69,25 @@ always_comb begin
 			o_random_out_w = LFSR_r[3:0];
 			next_time_w = next_time_r + period_r;
 			period_w = period_r + period_add;  //period double
+			// if Key_0 is pressed when in PROC state
+			if(i_start) begin
+				pause_w	 	= (~pause_r);
+			end
+			else begin
+				pause_w 	= pause_r;
+			end
 		end
 		else begin
 			o_random_out_w = o_random_out_r;
 			period_w       = period_r;
 			next_time_w    = next_time_r;
-		end
-
-		// if Key_0 is pressed when in PROC state
-		if(i_start) begin
-			pause_w	 	= (~pause_r);
-		end
-		else begin
-			pause_w 	= pause_r;
+			// if Key_0 is pressed when in PROC state
+			if(i_start) begin
+				pause_w	 	= (~pause_r);
+			end
+			else begin
+				pause_w 	= pause_r;
+			end
 		end
 	end
 	endcase
@@ -90,30 +96,30 @@ end
 // ===== Sequential Circuits =====
 always_ff @(posedge i_clk or negedge i_rst_n) begin
 	// reset
-	period_r       <= period_w;
-	next_time_r    <= next_time_w;
-	pause_r        <= pause_w;
 	if (!i_rst_n) begin
 		o_random_out_r <= 4'd0;
 		state_r        <= S_IDLE;
-		i_seed_r       <= (32'd10011);		
-		//seed 初始值
+		i_seed_r       <= {LFSR_w[28:0], 1'b1, 1'b0, 1'b1};     //seed 初始值
+		period_r       <= period_w;
+		next_time_r    <= next_time_w;
+		pause_r        <= pause_w;
+		LFSR_r 	       <= LFSR_w;
+		count_r        <= count_w;
 	end
 	else begin
 		o_random_out_r <= o_random_out_w;
 		state_r        <= state_w;
 		LFSR_r 	       <= {LFSR_w[28:0], LFSR_w[31]^LFSR_w[30]^LFSR_w[24]^LFSR_w[10], LFSR_w[31]^LFSR_w[29]^LFSR_w[23]^LFSR_w[9], LFSR_w[27]^LFSR_w[22]^LFSR_w[22]^LFSR_w[4]};
 		i_seed_r       <= i_seed_w;
-	end
-end
-
-always_ff @(posedge i_clk or negedge i_rst_n) begin
-	// counter state graph
-	if(state_w == S_PROC && !pause_w) begin
-		count_r 	  <= count_w + 1'd1;
-	end
-	else begin
-		count_r       <= count_w;
+		period_r       <= period_w;
+		next_time_r    <= next_time_w;
+		pause_r        <= pause_w;
+		if(state_w == S_PROC && !pause_w) begin
+			count_r     <= count_w + 1'd1;
+		end
+		else begin
+			count_r     <= count_w;
+		end
 	end
 end
 endmodule
