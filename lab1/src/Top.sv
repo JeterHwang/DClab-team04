b270b5a2c517fed2ae794da64ea45a84ab39e470
JeterHwang <45968071+JeterHwang@users.noreply.github.com>
2020-09-26 20:51:58 +0800

    same as master branch

--- lab1/src/Top.sv
+++ lab1/src/Top.sv
@@ -20,6 +20,7 @@ logic [3:0] o_random_out_r, o_random_out_w;
 
 // ===== Registers & Wires =====
 logic state_r, state_w;
+logic pause_r, pause_w;
 logic [31:0] LFSR_r, LFSR_w;
 logic [31:0] i_seed_r, i_seed_w;
 //Counter 
@@ -33,32 +34,61 @@ assign o_random_out = o_random_out_r;
 // ===== Combinational Circuits =====
 always_comb begin
 	// Default Values
-	o_random_out_w = o_random_out_r;
-	state_w        = state_r;
-	count_w 	   = count_r;
-	period_w       = period_r;
-	next_time_w    = next_time_r;
-	LFSR_w         = LFSR_r;
-	i_seed_w       = i_seed_r;
+
 	// FSM
 	case(state_r)
 	S_IDLE: begin
+		o_random_out_w = o_random_out_r;
+		i_seed_w       = i_seed_r;
 		if (i_start) begin
-			state_w        = S_PROC;
+			state_w = S_PROC;
 			//o_random_out_w = 4'd15;
-			count_w 	   = 32'b0;
-			period_w       = 32'b1000000;
+			count_w        = 32'd0;
+			period_w       = 32'd1000000;
+			next_time_w    = 32'd1000000;
 			LFSR_w         = i_seed_r;
+			pause_w        = 1'b0;
+		end
+		else begin
+			state_w        = state_r;
+			count_w        = count_r;
+			period_w       = period_r;
+			next_time_w    = next_time_r;
+			LFSR_w         = LFSR_r;
+			pause_w        = pause_r;
 		end
 	end
 	S_PROC: begin
-		state_w = (count_r > tmax) ? S_IDLE : state_w;
-		if(count_r >= next_time_r) begin
+		//o_random_out_w = 4'd15;
+		state_w 	 = (count_r > tmax) ? S_IDLE : state_r;
+		count_w 	 = count_r;
+		LFSR_w 		 = LFSR_r;
+		i_seed_w 	 = i_seed_r;
+		
+		if(count_r >= next_time_r && !pause_r) begin
 			o_random_out_w = LFSR_r[3:0];
-			next_time_w    = next_time_r + period_r;
-			period_w       = period_r + period_add;
+			next_time_w = next_time_r + period_r;
+			period_w = period_r + period_add;  //period double
+			// if Key_0 is pressed when in PROC state
+			if(i_start) begin
+				pause_w	 	= (~pause_r);
+			end
+			else begin
+				pause_w 	= pause_r;
+			end
 		end
-
+		else begin
+			o_random_out_w = o_random_out_r;
+			period_w       = period_r;
+			next_time_w    = next_time_r;
+			// if Key_0 is pressed when in PROC state
+			if(i_start) begin
+				pause_w	 	= (~pause_r);
+			end
+			else begin
+				pause_w 	= pause_r;
+			end
+		end
 	end
 	endcase
 end
@@ -66,29 +96,30 @@ end
 // ===== Sequential Circuits =====
 always_ff @(posedge i_clk or negedge i_rst_n) begin
 	// reset
-	period_r       <= period_w;
-	next_time_r    <= next_time_w;
 	if (!i_rst_n) begin
 		o_random_out_r <= 4'd0;
 		state_r        <= S_IDLE;
-		i_seed_r       <= (i_seed_w > 32'hdddddddd) ? (i_seed_w + 32'hab00045) : (32'h1000_0011);		
-		//seed 初始值
+		i_seed_r       <= {LFSR_w[28:0], 1'b1, 1'b0, 1'b1};     //seed 初始值
+		period_r       <= period_w;
+		next_time_r    <= next_time_w;
+		pause_r        <= pause_w;
+		LFSR_r 	       <= LFSR_w;
+		count_r        <= count_w;
 	end
 	else begin
 		o_random_out_r <= o_random_out_w;
 		state_r        <= state_w;
-		LFSR_r 	       <= {LFSR[31:3], LFSR[31]^LFSR[30]^LFSR[24]^LFSR[10], LFSR[31]^LFSR[29]^LFSR[23]^LFSR[9], LFSR[27]^LFSR[22]^LFSR[22]^LFSR[4]};
+		LFSR_r 	       <= {LFSR_w[28:0], LFSR_w[31]^LFSR_w[30]^LFSR_w[24]^LFSR_w[10], LFSR_w[31]^LFSR_w[29]^LFSR_w[23]^LFSR_w[9], LFSR_w[27]^LFSR_w[22]^LFSR_w[22]^LFSR_w[4]};
 		i_seed_r       <= i_seed_w;
-	end
-end
-
-always_ff @(posedge i_clk or negedge i_rst_n) begin
-	// counter state graph
-	if(state_w == S_PROC) begin
-		count_r 	  <= count_w + 1'b1;
-	end
-	else begin
-		count_r       <= count_w;
+		period_r       <= period_w;
+		next_time_r    <= next_time_w;
+		pause_r        <= pause_w;
+		if(state_w == S_PROC && !pause_w) begin
+			count_r     <= count_w + 1'd1;
+		end
+		else begin
+			count_r     <= count_w;
+		end
 	end
 end
 endmodule
