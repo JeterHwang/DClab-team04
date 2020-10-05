373fad89cc0fa268a45ecc3f5b3f63382cef1457
jeter <jeter@example.com>
2020-10-05 18:28:54 +0800

    add structure to RSA256core.sv

--- lab2/src/Rsa256Core.sv
+++ lab2/src/Rsa256Core.sv
@@ -8,8 +8,52 @@ module Rsa256Core (
 	output [255:0] o_a_pow_d, // plain text x
 	output         o_finished
 );
-
 // operations for RSA256 decryption
 // namely, the Montgomery algorithm
+// ======= states ======
+parameter S_IDLE = 2'd0;
+parameter S_PREP = 2'd1;
+parameter S_MONT = 2'd2;
+parameter S_CALC = 2'd3;
+
+// ======= registers & wires ======
+logic state_r, state_w;
+
+
+// ======= functions ======
+function logic [255:0] ModuloProduct(
+	input logic [255:0] N,b,
+	input logic [256:0] a,
+	input logic [9:0]   k
+);
+	pass
+endfunction
+
+function logic [255:0] MontgomeryAlgorithm(
+	input logic [255:0] N,a,b
+);
+	pass
+endfunction
+
+function logic [255:0] RSA256Mont(
+	input [255:0] N,y,d
+);
+	pass
+endfunction
+
+// ======= combinational circuit =======
+always_comb begin
+	
+end
+
+// ======= sequential circuit =========
+always_ff @(posedge i_clk or posedge i_rst) begin
+	
+end
 
 endmodule
+
+
+
+
+
