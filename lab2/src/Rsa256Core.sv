daee98414410af74a575b07ebef6fe7d49177ec5
JeterHwang <45968071+JeterHwang@users.noreply.github.com>
2020-10-07 19:20:37 +0800

    Update Rsa256Core.sv

--- lab2/src/Rsa256Core.sv
+++ lab2/src/Rsa256Core.sv
@@ -1,3 +1,8 @@
+package Rsa256;
+	`include "Montgomery.sv"
+	`include "ModuloProduct.sv"
+endpackage
+
 module Rsa256Core (
 	input          i_clk,
 	input          i_rst,
@@ -10,6 +15,7 @@ module Rsa256Core (
 );
 // operations for RSA256 decryption
 // namely, the Montgomery algorithm
+
 // ======= states ======
 parameter S_IDLE 	= 3'd0;
 parameter S_PREP 	= 3'd1;
@@ -69,7 +75,7 @@ ModuloProduct moduloproduct(
 	.i_start(i_start),
 	.N(i_n),
 	.b(i_y),
-	.a(257'd(1<<256)),
+	.a({1'b1, 256'b0}}),
 	.k(9'd256),
 	.t(trans_ini_w),
 	.prep_rd(prep_fin_w)
@@ -103,7 +109,7 @@ always_comb begin
 		S_PREMONT: begin
 			tra_start_w		= 1'b1;
 			state_w			= S_MONT;
-			if((d >> count_r) & 1) begin
+			if((i_d >> count_r) & 1) begin
 				mod_start_w = 1'b1;
 			end
 			else begin
@@ -119,7 +125,7 @@ always_comb begin
 				tra_start_w = 1'b0;
 			end
 			// mont finished
-			if(update_t_fin_r && (update_t_fin_r || ((d >> count_r) & 1))) begin
+			if(update_t_fin_r && (update_t_fin_r || ((i_d >> count_r) & 1))) begin
 				state_w 	= S_CALC;
 			end
 		end
@@ -177,198 +183,3 @@ always_ff @(posedge i_clk or posedge i_rst) begin
 	end
 end
 endmodule
-
-/***************************************/
-module ModuloProduct(
-    input          i_clk,
-    input          i_rst,
-    input          i_start,
-    input  [255:0] N,
-    input  [255:0] b,
-    input  [256:0] a,
-    input  [8:0]   k,
-    output [255:0] t,
-    output         prep_rd
-);
-// ====== states ========
-parameter S_IDLE  = 1'b0;
-parameter S_PREP  = 1'b1;
-// ====== regs & wires ======
-logic [255:0]   t_r, t_w;
-logic [255:0]   m_r, m_w;
-logic [8:0]     i_r, i_w;
-logic           state_r, state_w;
-logic           ready_r, ready_w;
-
-// ==== output assignment ====
-assign prep_rd = ready_r;
-assign t       = mr;
-
-// ====== combinational =====
-always_comb begin
-    case (state_r)    
-        S_IDLE: begin
-            if(i_start) begin
-                state_w = S_PREP;
-                t_w     = b;
-                m_w     = 256'd0;
-                i_w     = 9'd0;
-                ready_w = 0'd0;    
-            end
-            else begin
-                state_w = state_r;
-                t_w     = t_r;
-                m_w     = m_r;
-                i_w     = i_r;
-                ready_w = ready_r;
-            end    
-        end
-        S_PREP: begin
-            // update m
-            if((a >> i_r) & 1) begin
-                m_w = m_r + t_r - N;
-            end
-            else begin
-                m_w = m_r + t_r;
-            end
-            // update t
-            if(t_r + t_r > N) begin
-                t_w = t_r + t_r - N;
-            end
-            else begin
-                t_w = t_r + t_r;
-            end
-            // update state
-            if(i_r == k) begin
-                ready_w = 1'b1;
-                state_w = S_IDLE;
-            end
-            else begin
-                ready_w = ready_r;
-                state_w = state_r;
-            end
-            // update i
-            i_w = i_r + 9'd1;
-        end
-    endcase           
-end
-
-// ====== sequential ========
-always_ff @(posedge i_clk or posedge i_rst) begin
-    if(i_rst) begin
-        t_r     = 256'd0;
-        m_r     = 256'd0;
-        i_r     = 9'd0;
-        ready_r = 1'd0;
-        state_r = S_IDLE;
-    end
-    else begin
-        t_r     = t_w;
-        m_r     = m_w;
-        i_r     = i_w;
-        ready_r = ready_w;
-        state_r = state_w;
-    end
-end
-endmodule
-
-
-/*******************************/
-module Montgomery(
-    input           i_clk,
-    input           i_rst,
-    input           i_start,
-    input  [255:0]  N,
-    input  [255:0]  a,
-    input  [255:0]  b,
-    output [255:0]  m,
-    output          calc_rd
-);
-// ====== states ========
-parameter S_IDLE    = 2'd0;
-parameter S_PREP    = 2'd1;
-parameter S_ENDFOR  = 2'd2;
-// ====== regs & wires ======
-logic [255:0]   m_r, m_w;
-logic [8:0]     i_r, i_w;
-logic [1:0]     state_r, state_w;
-logic           ready_r, ready_w;
-
-// ==== output assignment ====
-assign calc_rd = ready_r;
-assign m       = mr;
-
-// ====== combinational ======
-always_comb begin
-    case (state_r)
-         S_IDLE: begin
-            if(i_start) begin
-                state_w = S_PREP;
-                m_w     = 256'd0;
-                i_w     = 9'd0;
-                ready_w = 1'b0;
-            end
-            else begin
-                state_w = state_r;
-                m_w     = m_r;
-                i_w     = i_r;
-                ready_w = ready_r;    
-            end
-        end
-        S_PREP: begin
-            ready_w = ready_r;
-            i_w     = i_r + 9'd1;
-            // update m
-            if(((a >> i_r) & 1) && ((m_r + b) & 1)) begin
-                m_w = ((m_r + b + N) >> 1);
-            end
-            else if (((a >> i_r) & 1) && !((m_r + b) & 1)) begin
-                m_w = ((m_r + b) >> 1);
-            end
-            else if (!((a >> i_r) & 1) && ((m_r + b) & 1)) begin
-                m_w = ((m_r + N) >> 1);
-            end
-            else begin
-                m_w = (m_r >> 1);
-            end
-            // update state
-            if(i_r == 9'd255) begin
-                state_w = S_ENDFOR;
-            end
-            else begin
-                state_w = state_r;
-            end
-        end
-        S_ENDFOR: begin
-            i_w = i_r;
-            if(m_r >= N) begin
-                m_w     = m_r - N;
-                state_w = state_r;
-                ready_w = ready_r;
-            end
-            else begin
-                m_w     = m_r;
-                state_w = S_IDLE;
-                ready_w = 1'b1;   
-            end
-        end
-    endcase    
-end
-
-// ====== sequential ========
-always_ff @(posedge i_clk or posedge i_rst) begin
-    if(i_rst) begin
-        m_r     = 256'd0;
-        i_r     = 9'd0;
-        ready_r = 1'd0;
-        state_r = S_IDLE;
-    end
-    else begin
-        m_r     = m_w;
-        i_r     = i_w;
-        ready_r = ready_w;
-        state_r = state_w;
-    end
-end
-endmodule
-
