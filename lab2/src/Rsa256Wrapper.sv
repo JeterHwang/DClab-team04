d4d95d641e3fa8bd76d15eb1ca018651310c4a37
JeterHwang <45968071+JeterHwang@users.noreply.github.com>
2020-10-09 20:35:44 +0800

    Update Rsa256Wrapper.sv

--- lab2/src/Rsa256Wrapper.sv
+++ lab2/src/Rsa256Wrapper.sv
@@ -16,16 +16,20 @@ localparam TX_OK_BIT   = 6;
 localparam RX_OK_BIT   = 7;
 
 // Feel free to design your own FSM!
-localparam S_GET_KEY = 0;
-localparam S_GET_DATA = 1;
-localparam S_WAIT_CALCULATE = 2;
-localparam S_SEND_DATA = 3;
+localparam S_READ_READY = 0;
+localparam S_GET_PU_KEY = 1;
+localparam S_GET_PR_KEY = 2;
+localparam S_GET_DATA = 3;
+localparam S_WAIT_CALCULATE = 4;
+localparam S_WRITE_READY = 5;
+localparam S_SEND_DATA = 6;
 
 logic [255:0] n_r, n_w, d_r, d_w, enc_r, enc_w, dec_r, dec_w;
 logic [1:0] state_r, state_w;
 logic [6:0] bytes_counter_r, bytes_counter_w;
 logic [4:0] avm_address_r, avm_address_w;
 logic avm_read_r, avm_read_w, avm_write_r, avm_write_w;
+logic [31:0] avm_readdata_r, avm_readdata_w;
 
 logic rsa_start_r, rsa_start_w;
 logic rsa_finished;
@@ -34,6 +38,7 @@ logic [255:0] rsa_dec;
 assign avm_address = avm_address_r;
 assign avm_read = avm_read_r;
 assign avm_write = avm_write_r;
+assign avm_readdata = avm_readdata_r;
 assign avm_writedata = dec_r[247-:8];
 
 Rsa256Core rsa256_core(
@@ -65,7 +70,83 @@ task StartWrite;
 endtask
 
 always_comb begin
-    // TODO
+    n_w                 = n_r;
+    d_w                 = d_r;
+    enc_w               = enc_r;
+    dec_w               = dec_r;
+    avm_address_w       = avm_address_r;
+    avm_read_w          = avm_read_r;
+    avm_write_w         = avm_write_r;
+    state_w             = state_r;
+    bytes_counter_w     = 1'b0;
+    rsa_start_w         = rsa_start_r;
+    avm_readdata_w      = avm_readdata_r;
+    case (state_r)
+		S_READ_READY: begin
+            if(avm_readdata_w[RX_OK_BIT] == 1'd1) begin
+                if(avm_waitrequest == 1'd0) begin
+                    state_w = S_GET_DATA;
+                    StartRead(RX_BASE);
+                end
+            end
+		end
+		S_GET_PU_KEY: begin
+            n_w = avm_readdata_w[(bytes_counter_w << 3)+7 : (bytes_counter_w << 3)];
+            bytes_counter_w = bytes_counter_w + 1'b1;
+            if(bytes_counter_w == 32) begin
+                state_w = S_GET_PR_KEY;
+                bytes_counter_w = 1'b0;
+            end
+		end
+        S_GET_PR_KEY: begin
+            d_w = avm_readdata_w[(bytes_counter_w << 3)+7 : (bytes_counter_w << 3)];
+            bytes_counter_w = bytes_counter_w + 1'b1;
+            if(bytes_counter_w == 32) begin
+                state_w = S_GET_DATA;
+                bytes_counter_w = 1'b0;
+            end
+		end
+        S_GET_DATA: begin
+            enc_w = avm_readdata_w[(bytes_counter_w << 3)+7 : (bytes_counter_w << 3)];
+            bytes_counter_w = bytes_counter_w + 1'b1;
+            if(bytes_counter_w == 32) begin
+                state_w = S_WAIT_CALCULATE;
+                bytes_counter_w = 1'b0;
+                rsa_start_w = 1;
+            end
+		end
+		S_WAIT_CALCULATE: begin
+            avm_read_w = 1'd0;
+            if(rsa_finished) begin
+                if(avm_waitrequest == 1'd0) begin
+                    rsa_start_w = 1'd0;
+                    dec_w = rsa_dec;
+                    state_w = S_WRITE_READY;
+                    avm_address_w = STATUS_BASE;
+                end
+            end
+		end
+		S_WRITE_READY: begin
+			if(avm_readdata_w[TX_OK_BIT] == 1) begin
+                if(avm_waitrequest == 1'd0) begin
+                    state_w = S_SEND_DATA;
+                    StartWrite(TX_BASE);
+                    bytes_counter_w = 1'b0;
+                end
+            end
+        end
+		S_SEND_DATA: begin
+            if(avm_waitrequest == 1'd0) begin
+                avm_writedata[7:0] = dec_w[(bytes_counter_w << 3)+7 : (bytes_counter_w << 3)];
+                bytes_counter_w = bytes_counter_w + 1'b1;
+                if(bytes_counter_w == 31) begin
+                    state_w = S_READ_READY;
+                    bytes_counter_w = 1'b0;
+                    avm_address_w = STATUS_BASE;
+                end
+            end
+        end
+	endcase
 end
 
 always_ff @(posedge avm_clk or posedge avm_rst) begin
@@ -77,9 +158,10 @@ always_ff @(posedge avm_clk or posedge avm_rst) begin
         avm_address_r <= STATUS_BASE;
         avm_read_r <= 1;
         avm_write_r <= 0;
-        state_r <= S_GET_KEY;
+        state_r <= S_READ_READY;
         bytes_counter_r <= 63;
         rsa_start_r <= 0;
+        avm_readdata_r <= 32'd0;
     end else begin
         n_r <= n_w;
         d_r <= d_w;
@@ -91,6 +173,7 @@ always_ff @(posedge avm_clk or posedge avm_rst) begin
         state_r <= state_w;
         bytes_counter_r <= bytes_counter_w;
         rsa_start_r <= rsa_start_w;
+        avm_readdata_r <= avm_readdata_w;
     end
 end
 
