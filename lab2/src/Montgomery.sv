5fb35b994a977084dcad619e00a114e528fbdce8
JeterHwang <45968071+JeterHwang@users.noreply.github.com>
2020-10-12 12:55:20 +0800

    Update Montgomery.sv

--- lab2/src/Montgomery.sv
+++ lab2/src/Montgomery.sv
@@ -9,13 +9,12 @@ module Montgomery(
     output          calc_rd
 );
 // ====== states ========
-parameter S_IDLE    = 2'd0;
-parameter S_PREP   = 2'd1;
-parameter S_CALC    = 2'd2;
+parameter S_IDLE    = 1'd0;
+parameter S_CALC    = 1'd1;
 // ====== regs & wires ======
 logic [257:0]   m_r, m_w;
 logic [8:0]     i_r, i_w;
-logic [1:0]     state_r, state_w;
+logic           state_r, state_w;
 logic           ready_r, ready_w;
 
 // ==== output assignment ====
@@ -31,39 +30,55 @@ always_comb begin
     case (state_r)
         S_IDLE: begin
             if(i_start) begin
-                state_w = S_PREP;
+                state_w = S_CALC;
                 m_w     = 258'd0;
                 i_w     = 9'd0;
                 ready_w = 1'b0;
             end
         end
-        S_PREP: begin
-            state_w = S_CALC;
-            if((a[i_r] & 1) && ((m_r + b) & 258'd1)) begin
-                m_w = ((m_r + b + N) >> 1);
-            end
-            else if ((a[i_r] & 1) && !((m_r + b) & 258'd1)) begin
-                m_w = ((m_r + b) >> 1);
-            end
-            else if (!(a[i_r] & 1) && (m_r[0] & 1)) begin
-                m_w = ((m_r + N) >> 1);
-            end
-            else begin
-                m_w = (m_r >> 1);
-            end
-        end
         S_CALC: begin
-            if(i_r == 9'd255) begin // end for loop
+            if(i_r == 9'd255) begin
                 state_w = S_IDLE;
                 ready_w = 1'b1;
-                // final update m
-                if(m_r >= N) begin
-                    m_w     = m_r - N;
+                if((a[i_r] & 1) && ((m_r + b) & 258'd1)) begin
+                    if(((m_r + b + N) >> 1) >= N)
+                        m_w = ((m_r + b + N) >> 1) - N;
+                    else 
+                        m_w = ((m_r + b + N) >> 1);
+                end
+                else if ((a[i_r] & 1) && !((m_r + b) & 258'd1)) begin
+                    if(((m_r + b) >> 1) >= N)
+                        m_w = ((m_r + b) >> 1) - N;
+                    else
+                        m_w = ((m_r + b) >> 1);
+                end
+                else if (!(a[i_r] & 1) && (m_r[0] & 1)) begin
+                    if(((m_r + N) >> 1) >= N)
+                        m_w = ((m_r + N) >> 1) - N;
+                    else
+                        m_w = ((m_r + N) >> 1);
+                end
+                else begin
+                    if((m_r >> 1) >= N)
+                        m_w = (m_r >> 1) - N;
+                    else
+                        m_w = (m_r >> 1);
                 end
             end
-            else begin // keep iterating
-                state_w = S_PREP;
-                i_w     = i_r + 9'd1;    
+            else begin
+                if((a[i_r] & 1) && ((m_r + b) & 258'd1)) begin
+                    m_w = ((m_r + b + N) >> 1);
+                end
+                else if ((a[i_r] & 1) && !((m_r + b) & 258'd1)) begin
+                    m_w = ((m_r + b) >> 1);
+                end
+                else if (!(a[i_r] & 1) && (m_r[0] & 1)) begin
+                    m_w = ((m_r + N) >> 1);
+                end
+                else begin
+                    m_w = (m_r >> 1);
+                end
+                i_w = i_r + 9'd1;    
             end
         end
     endcase    
