67c31307cbd619779f79fe38715d2ff592c2804c
BillyWu0730 <b07901106@ntu.edu.tw>
2020-10-29 14:33:28 +0800

    initialized

---
+++ lab3/src/I2cInitializer.sv
@@ -0,0 +1,21 @@
+module I2cIntializer(
+    input i_rst_n,
+    input i_clk,
+    input i_start,
+    output o_finished,
+    output o_sclk,
+    output o_sdat,
+    output O_oen
+);
+
+
+
+always_comb begin
+    
+end
+
+always_ff @(posedge ck, posedge arst) begin
+    
+end
+
+endmodule
