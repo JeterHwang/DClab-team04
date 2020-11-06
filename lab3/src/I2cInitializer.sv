c39ae5c4557b1ad05d588a7c2f7ca50a189e8049
JeterHwang <jeter0309@gmail.com>
2020-11-07 02:31:11 +0800

    update I2C_tb.sv

--- lab3/src/I2cInitializer.sv
+++ lab3/src/I2cInitializer.sv
@@ -12,23 +12,21 @@ parameter S_IDLE    = 3'd0;
 parameter S_BUFFER  = 3'd1;
 parameter S_BLUE    = 3'd2;
 parameter S_GREEN   = 3'd3;
-parameter S_ACKING  = 3'd4;
-parameter S_BLUE2   = 3'd5;
-parameter S_DELAY   = 3'd6;
-parameter S_DONE    = 3'd7;
+parameter S_DELAY   = 3'd4;
+parameter S_DONE    = 3'd5;
 
 parameter bit[23:0] INIT_DATA [6:0] = {
-	24'b0011_0100_000_1111_0_0000_0000, // Reset
+	27'b0011_0100_1_000_1111_0_1_0000_0000_1, // Reset
 	// 24'b0011_0100_000_0000_0_1001_0111, // Left Line In
 	// 24'b0011_0100_000_0001_0_1001_0111,	// Right Line In
 	// 24'b0011_0100_000_0010_0_0111_1001, // Left Headphone Out 
 	// 24'b0011_0100_000_0011_0_0111_1001,	// Right Headphone Out
-	24'b0011_0100_000_0100_0_0001_0101, // Analogue Audio Path Control
-	24'b0011_0100_000_0101_0_0000_0000, // Digital Audio Path Control
-	24'b0011_0100_000_0110_0_0000_0000, // Power Down Control
-	24'b0011_0100_000_0111_0_0100_0010, // Digital Audio Interface Format
-	24'b0011_0100_000_1000_0_0001_1001, // Sampling Control
-	24'b0011_0100_000_1001_0_0000_0001  // Active Control
+	27'b0011_0100_1_000_0100_0_1_0001_0101_1, // Analogue Audio Path Control
+	27'b0011_0100_1_000_0101_0_1_0000_0000_1, // Digital Audio Path Control
+	27'b0011_0100_1_000_0110_0_1_0000_0000_1, // Power Down Control
+	27'b0011_0100_1_000_0111_0_1_0100_0010_1, // Digital Audio Interface Format
+	27'b0011_0100_1_000_1000_0_1_0001_1001_1, // Sampling Control
+	27'b0011_0100_1_000_1001_0_1_0000_0001_1  // Active Control
 };
 
 
@@ -69,36 +67,22 @@ always_comb begin
         end
         S_BLUE: begin
             SCL_w       = 1'b1 ; 
-            if(counts_r == 8 || counts_r == 16 || counts_r == 24) begin
-                state_w     = S_ACKING;
+            if(counts_r == 8 || counts_r == 17 || counts_r == 26) 
                 oen_w       = 1'b0;
-            end
-            else begin
+            else
+                oen_w       = 1'b1;
+            if(counts_r == 27)
+                state_w     = S_DELAY;
+            else
                 state_w     = S_GREEN;
-                counts_w    = counts_r + 1;
-            end
+            counts_w    = counts_r + 1;
         end
         S_GREEN: begin
-            state_w     = S_BLUE;
             SCL_w       = 1'b0;
-            SDA_w       = INIT_DATA[init_r][23 - counts_r];
-        end
-        S_ACKING: begin
-            SCL_w       = 1'b0;
-            state_w     = S_BLUE;
             oen_w       = 1'b1 ;
-            if(counts_r == 24) begin
-                SDA_w       = 1'b0;
-                state_w     = S_BLUE2;
+            SDA_w       = INIT_DATA[init_r][26 - counts_r];
+            state_w     = S_BLUE;
             end
-            else begin
-                SDA_w       = INIT_DATA[init_r][23 - counts_r];
-                state_w     = S_BLUE;
-            end
-        end
-        S_BLUE2: begin
-            state_w     = S_DELAY;
-            SCL_w       = 1'b1;
         end
         S_DELAY: begin
             state_w     = S_DONE;
@@ -135,7 +119,7 @@ always_ff @(posedge i_clk) begin
         SCL_r       <= SCL_w;
         SDA_r       <= SDA_w;
         counts_r    <= counts_w;
-        init_r     <= init_w;
+        init_r      <= init_w;
         oen_r       <= oen_w;
     end
 end
