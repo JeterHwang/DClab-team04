abf3ffe7a9c77917169b6dc6af08ac908a5b8028
jeter <jeter@example.com>
2020-11-07 11:04:51 +0800

    update I/O

--- lab3/src/AudDSP.sv
+++ lab3/src/AudDSP.sv
@@ -1,17 +1,17 @@
 module AudDSP(
-    input   i_rst_n,
-    input   i_clk,
-    input   i_start,
-    input   i_pause,
-    input   i_stop,
-    input   i_speed,
-    input   i_fast,
-    input   i_slow_0,
-    input   i_slow_1,
-    input   i_daclrck,
-    input   i_sram_data,
-    output  o_dac_data,
-    output  o_addr_play
+    input       i_rst_n,
+    input       i_clk,
+    input       i_start,
+    input       i_pause,
+    input       i_stop,
+    input [3:0] i_speed,
+    input       i_fast,
+    input       i_slow_0,
+    input       i_slow_1,
+    input       i_daclrck,
+    input       i_sram_data,
+    output      o_dac_data,
+    output      o_addr_play
 );
 
 always_comb begin
