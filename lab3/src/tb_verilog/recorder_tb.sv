86f09c9cb6f6561f67c53aa4a3fa03b15babbf01
Peter <xxia52122@gmail.com>
2020-11-09 01:52:54 +0800

    new

--- lab3/src/tb_verilog/recorder_tb.sv
+++ lab3/src/tb_verilog/recorder_tb.sv
@@ -62,18 +62,15 @@ module recorder_tb;
                 state   = i;
             end
             @(negedge bclk);
-            $display("%16b", data_arr);
-            $display("%16b", out);
-            $display("%16b", ans);
             for(int j = 0; j < 16; j++) begin
                 data = data_arr[15-j];
                 @(negedge bclk);
-                    $display("%16b", out);
                     $display("%16b", ans);
                     ans = ((ans << 1) | (out<<1));
-                    $display("%16b", data_arr);
+                    
                     $display("%16b", out);
                     $display("%16b", ans);
+                    $display("%16b", data_arr);
                     $display("%1b %1b %1b", out[15-j], data, ans[15-j]);
                     $display("++++++++++++++++++++");
                 if (j == 5) begin
