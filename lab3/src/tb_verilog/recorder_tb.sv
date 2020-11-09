a3b0fa4ee3d48d6bce3182fe51ff815c262dc712
Peter <xxia52122@gmail.com>
2020-11-09 21:57:22 +0800

    new

--- lab3/src/tb_verilog/recorder_tb.sv
+++ lab3/src/tb_verilog/recorder_tb.sv
@@ -60,8 +60,8 @@ module recorder_tb;
         #(`CYCLE*2) rst = 1;
         #(`CYCLE*2) rst = 0;
         for(int i = 0; i < 65000; i++) begin
-            #(`CYCLE*8) start = 1;
-            #(`CYCLE*8) start = 0;
+            #(`CYCLE*15) start = 1;
+            #(`CYCLE*5) start = 0;
             
             @(negedge lr_clk) begin
                 in      = data_arr[i];
