208c7be218c5385722ec0a2bc81ed199299ff3ea
hsiehjackson <fanoping@gmail.com>
2020-11-10 14:59:37 +0800

    debug message

--- lab3/src/tb_verilog/INCA_libs/irun.lnx8664.12.20.nc/.timestamp.ts
+++ lab3/src/tb_verilog/INCA_libs/irun.lnx8664.12.20.nc/.timestamp.ts
@@ -2,3 +2,7 @@
 1604689973 /home/team04/jeter/DClab-team04/lab3/src/I2cInitializer.sv
 1604825685 /home/team04/jeter/DClab-team04/lab3/src/tb_verilog/player_tb.sv
 1604824684 /home/team04/jeter/DClab-team04/lab3/src/AudPlayer.sv
+1604991442 /home/team04/jeter/DClab-team04/lab3/src/tb_verilog/LCD_tb.sv
+1604991442 /home/team04/jeter/DClab-team04/lab3/src/LCDTop.sv
+1604991442 /home/team04/jeter/DClab-team04/lab3/src/LCDdatacontroll.sv
+1604991442 /home/team04/jeter/DClab-team04/lab3/src/LCDinstructions.sv
