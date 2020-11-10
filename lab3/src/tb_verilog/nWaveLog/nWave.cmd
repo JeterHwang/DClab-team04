b217b012dfb8c47a92e31e60b27a1c95aafc1922
hsiehjackson <fanoping@gmail.com>
2020-11-10 15:03:50 +0800

    debug message

--- lab3/src/tb_verilog/nWaveLog/nWave.cmd
+++ lab3/src/tb_verilog/nWaveLog/nWave.cmd
@@ -1,91 +1,45 @@
+wvResizeWindow -win $_nWave1 79 236 1840 1016
 wvSetPosition -win $_nWave1 {("G1" 0)}
 wvOpenFile -win $_nWave1 \
-           {/home/team04/jeter/DClab-team04/lab3/src/tb_verilog/player.fsdb}
+           {/home/team04/jeter/DClab-team04/lab3/src/tb_verilog/LCD.fsdb}
 wvGetSignalOpen -win $_nWave1
-wvGetSignalSetScope -win $_nWave1 "/player_tb"
-wvSetPosition -win $_nWave1 {("G1" 8)}
-wvSetPosition -win $_nWave1 {("G1" 8)}
+wvGetSignalSetScope -win $_nWave1 "/LCD_tb"
+wvGetSignalSetScope -win $_nWave1 "/LCD_tb/LCD"
+wvSetPosition -win $_nWave1 {("G1" 7)}
+wvSetPosition -win $_nWave1 {("G1" 7)}
 wvAddSignal -win $_nWave1 -clear
 wvAddSignal -win $_nWave1 -group {"G1" \
-{/player_tb/ans\[15:0\]} \
-{/player_tb/bclk} \
-{/player_tb/data\[15:0\]} \
-{/player_tb/enable} \
-{/player_tb/lr_clk} \
-{/player_tb/out} \
-{/player_tb/rst} \
-{/player_tb/state\[2:0\]} \
+{/LCD_tb/LCD/i_clk_800k} \
+{/LCD_tb/LCD/i_rst_n} \
+{/LCD_tb/LCD/i_start} \
+{/LCD_tb/LCD/inst_start_r} \
+{/LCD_tb/LCD/o_init_finish} \
+{/LCD_tb/LCD/state_r\[2:0\]} \
+{/LCD_tb/LCD/write_start_r} \
 }
 wvAddSignal -win $_nWave1 -group {"G2" \
 }
-wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 7 8 )} 
-wvSetPosition -win $_nWave1 {("G1" 8)}
-wvGetSignalSetScope -win $_nWave1 "/player_tb/player0"
-wvSetPosition -win $_nWave1 {("G1" 18)}
-wvSetPosition -win $_nWave1 {("G1" 18)}
+wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 7 )} 
+wvSetPosition -win $_nWave1 {("G1" 7)}
+wvSetPosition -win $_nWave1 {("G1" 7)}
+wvSetPosition -win $_nWave1 {("G1" 7)}
 wvAddSignal -win $_nWave1 -clear
 wvAddSignal -win $_nWave1 -group {"G1" \
-{/player_tb/ans\[15:0\]} \
-{/player_tb/bclk} \
-{/player_tb/data\[15:0\]} \
-{/player_tb/enable} \
-{/player_tb/lr_clk} \
-{/player_tb/out} \
-{/player_tb/rst} \
-{/player_tb/state\[2:0\]} \
-{/player_tb/player0/aud_dacdat_r\[15:0\]} \
-{/player_tb/player0/counter_r\[3:0\]} \
-{/player_tb/player0/i_bclk} \
-{/player_tb/player0/i_dac_data\[15:0\]} \
-{/player_tb/player0/i_daclrck} \
-{/player_tb/player0/i_en} \
-{/player_tb/player0/i_rst_n} \
-{/player_tb/player0/o_aud_dacdat} \
-{/player_tb/player0/o_aud_dactdat} \
-{/player_tb/player0/state_r\[1:0\]} \
+{/LCD_tb/LCD/i_clk_800k} \
+{/LCD_tb/LCD/i_rst_n} \
+{/LCD_tb/LCD/i_start} \
+{/LCD_tb/LCD/inst_start_r} \
+{/LCD_tb/LCD/o_init_finish} \
+{/LCD_tb/LCD/state_r\[2:0\]} \
+{/LCD_tb/LCD/write_start_r} \
 }
 wvAddSignal -win $_nWave1 -group {"G2" \
 }
-wvSelectSignal -win $_nWave1 {( "G1" 9 10 11 12 13 14 15 16 17 18 )} 
-wvSetPosition -win $_nWave1 {("G1" 18)}
-wvSetPosition -win $_nWave1 {("G1" 18)}
-wvSetPosition -win $_nWave1 {("G1" 18)}
-wvAddSignal -win $_nWave1 -clear
-wvAddSignal -win $_nWave1 -group {"G1" \
-{/player_tb/ans\[15:0\]} \
-{/player_tb/bclk} \
-{/player_tb/data\[15:0\]} \
-{/player_tb/enable} \
-{/player_tb/lr_clk} \
-{/player_tb/out} \
-{/player_tb/rst} \
-{/player_tb/state\[2:0\]} \
-{/player_tb/player0/aud_dacdat_r\[15:0\]} \
-{/player_tb/player0/counter_r\[3:0\]} \
-{/player_tb/player0/i_bclk} \
-{/player_tb/player0/i_dac_data\[15:0\]} \
-{/player_tb/player0/i_daclrck} \
-{/player_tb/player0/i_en} \
-{/player_tb/player0/i_rst_n} \
-{/player_tb/player0/o_aud_dacdat} \
-{/player_tb/player0/o_aud_dactdat} \
-{/player_tb/player0/state_r\[1:0\]} \
-}
-wvAddSignal -win $_nWave1 -group {"G2" \
-}
-wvSelectSignal -win $_nWave1 {( "G1" 9 10 11 12 13 14 15 16 17 18 )} 
-wvSetPosition -win $_nWave1 {("G1" 18)}
+wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 7 )} 
+wvSetPosition -win $_nWave1 {("G1" 7)}
 wvGetSignalClose -win $_nWave1
-wvResizeWindow -win $_nWave1 79 26 1840 1016
-wvResizeWindow -win $_nWave1 79 26 1840 1016
-wvSetPosition -win $_nWave1 {("G2" 0)}
-wvSelectGroup -win $_nWave1 {G2}
-wvSetPosition -win $_nWave1 {("G1" 8)}
-wvMoveSelected -win $_nWave1
-wvSetPosition -win $_nWave1 {("G1" 8)}
-wvSetPosition -win $_nWave1 {("G1" 19)}
-wvSetPosition -win $_nWave1 {("G1" 19)}
-wvSetPosition -win $_nWave1 {("G1" 8)}
-wvSelectGroup -win $_nWave1 {G2}
-wvSelectSignal -win $_nWave1 {( "G1" 16 )} 
+wvZoom -win $_nWave1 0.000000 275229.638702
+wvZoom -win $_nWave1 0.000000 18554.807103
+wvResizeWindow -win $_nWave1 79 236 1840 1016
+wvResizeWindow -win $_nWave1 79 236 1840 1016
 wvSelectGroup -win $_nWave1 {G2}
