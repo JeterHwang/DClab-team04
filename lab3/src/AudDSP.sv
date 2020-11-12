2a4996bc534da3b0610f6c7e3cab039ad93c526a
JeterHwang <45968071+JeterHwang@users.noreply.github.com>
2020-11-12 17:01:41 +0800

    Update AudDSP.sv

--- lab3/src/AudDSP.sv
+++ lab3/src/AudDSP.sv
@@ -38,6 +38,7 @@ logic signed [15:0] sram_r1, sram_r2, sram_w1, sram_w2; // store consecutive sra
 logic [3:0] interpolation_counter_r, interpolation_counter_w; // interpolation count
 logic [1:0] slow_fetch_counter_r, slow_fetch_counter_w;
 logic signed [15:0] step;
+logic start_r,start_w,pause_r,pause_w;
 
 
 assign o_sram_addr = addr_r;
@@ -48,7 +49,8 @@ assign step = ($signed(sram_r2) - $signed(sram_r1)) / $signed(speed_r);
 
 
 always_comb begin
-    state_w = state_r;
+	start_w = i_start;
+	pause_w = i_pause;
 	case(state_r)
 		S_IDLE:
 			begin
@@ -60,20 +62,25 @@ always_comb begin
 				player_en_w = 0;
 				addr_w = 0;
 				dac_data_w = 0;
-				// done
+				// finished
 				if(i_stop)
 					finished_w = 1;
 				else
 					finished_w = 0;
 				// FSM
-				if(i_start && !i_pause)	begin
+
+
+				if(start_w && !start_r)	begin
 					if(i_fast)
 						state_w = S_FAST_FETCH;
 					else if(i_slow_0)
 						state_w = S_SLOW_0_FETCH;
 					else if(i_slow_1)
 						state_w = S_SLOW_1_FETCH;
+					else
+						state_w = state_r;
 				end
+				else state_w = state_r;
 			end
         
 		S_FAST_FETCH:
@@ -82,7 +89,7 @@ always_comb begin
 				sram_w2 = sram_r2;
 				slow_fetch_counter_w = 0;
 				interpolation_counter_w = 0;
-				// done
+				// finished
 				if(i_stop)
 					finished_w = 1;
 				else
@@ -101,7 +108,7 @@ always_comb begin
 				end
 				else begin
 					dac_data_w = i_sram_data;
-					if(i_daclrck && !i_pause)
+					if(i_daclrck && (!i_pause))
 						player_en_w = 1;
 					else
 						player_en_w = 0;
@@ -479,6 +486,8 @@ always_ff @(posedge i_clk) begin
 		slow_fetch_counter_r <= 0;
 		max_count <= 0;
 		min_count <= 0;
+		start_r <= 0;
+		pause_r <= 0;
 	end
 	else begin
 		state_r <= state_w;
@@ -494,6 +503,8 @@ always_ff @(posedge i_clk) begin
 		slow_fetch_counter_r <= slow_fetch_counter_w;
 		max_count <= (speed_r * (i_record_counter - 1)) + 1; 
 		min_count <= i_record_counter / speed_r;
+		start_r <= start_w;
+		pause_r <= pause_w;
 	end
 end
 endmodule
