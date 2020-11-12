e417b5274171123fb3d8c41714da0b95b8a27593
hsiehjackson <fanoping@gmail.com>
2020-11-12 20:49:13 +0800

    update syntax

--- lab3/src/tb_verilog/Top_tb/INCA_libs/irun.lnx8664.12.20.nc/.timestamp.ts
+++ lab3/src/tb_verilog/Top_tb/INCA_libs/irun.lnx8664.12.20.nc/.timestamp.ts
@@ -1,7 +1,7 @@
 1605182625 /home/team04/jeter/DClab-team04/lab3/src/tb_verilog/Top_tb/Top_tb.sv
 1605178255 /home/team04/jeter/DClab-team04/lab3/src/Top.sv
 1605183527 /home/team04/jeter/DClab-team04/lab3/src/I2cInitializer.sv
-1605182808 /home/team04/jeter/DClab-team04/lab3/src/AudDSP.sv
+1605184622 /home/team04/jeter/DClab-team04/lab3/src/AudDSP.sv
 1605178425 /home/team04/jeter/DClab-team04/lab3/src/AudPlayer.sv
 1605170848 /home/team04/jeter/DClab-team04/lab3/src/AudRecorder.sv
 1605183785 /home/team04/jeter/DClab-team04/lab3/src/LCDTop.sv
