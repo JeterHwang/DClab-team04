b867f48d4ae9f0fc177be032876a0a7668b5348d
hsiehjackson <fanoping@gmail.com>
2020-12-17 22:18:11 +0800

    update

---
+++ final-project/gobang/tb_verilog/INCA_libs/irun.lnx8664.12.20.nc/.timestamp.ts
@@ -0,0 +1,1 @@
+1608214660 /home/team04/jeter/DClab-team04/final-project/gobang/tb_verilog/PG_tb.sv
--- final-project/gobang/tb_verilog/PG_tb.sv
+++ final-project/gobang/tb_verilog/PG_tb.sv
@@ -14,29 +14,48 @@ module PG_tb;
     logic [399:0] Y_output;
     logic [8:0] buffer_size;
     logic PG_finish;
+    int fp_i, fp_o;
+    int buffer[15];
+    int status;
 
-    point_generator PG(
-        .i_clk(clk),
-        .i_rst_n(rst_n),
-        .i_start(start),
-        .i_board(i_board),
-        .o_posX(X_output),
-        .o_posY(Y_output),
-        .o_size(buffer_size),
-        .o_finish(PG_finish)
-    );
-    always #HCLK CLK = ~CLK;
+    //point_generator PG(
+    //    .i_clk(clk),
+    //    .i_rst_n(rst_n),
+    //    .i_start(start),
+    //    .i_board(i_board),
+    //    .o_posX(X_output),
+    //    .o_posY(Y_output),
+    //    .o_size(buffer_size),
+    //    .o_finish(PG_finish)
+    //);
+
+    initial clk = 0;
+    always #HCLK clk = ~clk;
 
     initial begin
         $fsdbDumpfile("PointGenerator.fsdb");
         $fsdbDumpvars;
         fp_i = $fopen("../pattern/PG_test1_i.txt");
-        fp_o = $fopen("../pattern/PG_test1_o.txt");
+        fp_o = $fopen("../output/PG_test1_o.txt");
         
-        for(int i = 0; i < 15; i++) begin
-            $fscanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", board[15* i], board[15* i + 1], board[15* i + 2], board[15* i + 3], board[15* i + 4], board[15* i + 5], board[15* i + 6], board[15* i + 7], board[15* i + 8], board[15* i + 9], board[15* i + 10], board[15* i + 11], board[15* i + 12], board[15* i + 13], board[15* i + 14]);
-        end
-        $display("%d", board[0]);
+        if(fp_i) 
+            $display("File was opened successfully : %0d", fp_i);
+        else
+            $display("File was not opened successfully : %0d", fp_i);
+
+        status = $fscanf(fp_i, "%0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d ", i_board[0], i_board[1], i_board[2], i_board[3], i_board[4], i_board[5], i_board[6], i_board[7], i_board[8], i_board[9], i_board[10], i_board[11], i_board[12], i_board[13], i_board[14]);
+        //for(int i = 0; i < 15; i++) begin
+        //    status = $fscanf(fp_i, "%0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d ", i_board[i * 15 + 0], i_board[i * 15 + 1], i_board[i * 15 + 2], i_board[i * 15 + 3], i_board[i * 15 + 4], i_board[i * 15 + 5], i_board[i * 15 + 6], i_board[i * 15 + 7], i_board[i * 15 + 8], i_board[i * 15 + 9], i_board[i * 15 + 10], i_board[i * 15 + 11], i_board[i * 15 + 12], i_board[i * 15 + 13], i_board[i * 15 + 14]);
+        //    if(status != 15) begin
+        //        $display("Error !!");
+        //    end
+        //end
+        
+        //for(int i = 0; i < 15; i++) begin
+        //    $display("%0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d %0d", i_board[i * 15 + 0], i_board[i * 15 + 1], i_board[i * 15 + 2], i_board[i * 15 + 3], i_board[i * 15 + 4], i_board[i * 15 + 5], i_board[i * 15 + 6], i_board[i * 15 + 7], i_board[i * 15 + 8], i_board[i * 15 + 9], i_board[i * 15 + 10], i_board[i * 15 + 11], i_board[i * 15 + 12], i_board[i * 15 + 13], i_board[i * 15 + 14]);
+        //end
+        $finish;
+
         rst_n = 1;
         #CLK rst_n = 0;
         rst_n = 1;
