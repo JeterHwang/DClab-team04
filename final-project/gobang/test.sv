ce92572351c27b0da1d054519e1dd1d5bca69b2a
peter <xxia52122@gmail.com>
2020-12-19 21:10:50 +0800

    add score

--- final-project/gobang/test.sv
+++ final-project/gobang/test.sv
@@ -5,27 +5,51 @@ module Score (
                   
 	output [24:0] out
 );
+parameter S_IDLE = 1'd0;
+parameter S_FINISH = 1'd1;
+logic [31:0] count_w, count_r;
 logic [31:0] state_w, state_r;
 
-always_comb begin
-    for(int i=0; i<=2; i++) begin
-        state_w = i;
-    end
-    for(int j=0; j<=6; i++) begin
-        state_w = i;
-    end
+task ff;
 
+input [31:0] count;
+        begin
+            for(int i=0; i<=2; i++) begin
+                count[31 - i] = i;
+            end
+            for(int j=0; j<=6; j++) begin
+                count[31 - i] = j;
+            end
+
+
+        end
+endtask
+always_comb begin
+    state_w         = state_r;
+    count_w         = count_r;
+    case(state_r)
+        S_IDLE: begin
+            ff(count_w);
+            state_w = S_FINISH;
+        end
+        S_FINISH: begin
+            state_w = S_IDLE;
+
+        end
+    endcase
 
 
 end
 
 always_ff @(negedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
-        state_r         <= 0;
+        state_r         <= S_IDLE;
+        count_r         <= 0;
 
     end
     else begin
         state_r         <= state_w;
+        count_r         <= count_w;
 
     end
 end
