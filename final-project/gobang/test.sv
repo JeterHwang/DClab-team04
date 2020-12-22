c2d4fe06544b60f162960a76827d970bcdb4c2a6
peter <xxia52122@gmail.com>
2020-12-22 19:24:31 +0800

    fix mobilePay error

--- final-project/gobang/test.sv
+++ final-project/gobang/test.sv
@@ -9,6 +9,8 @@ parameter S_IDLE = 1'd0;
 parameter S_FINISH = 1'd1;
 logic [4:0] count_w, count_r;
 logic [4:0] no_w, no_r;
+logic [4:0] counter_w, counter_r;
+logic [4:0] new_w, new_r;
 logic  state_w, state_r;
 assign out = count_r;
 
@@ -19,9 +21,13 @@ task ff;
             begin
                 for(int i=0; i<=2; i++) begin
                     count_w = data;
+                    counter_w  = counter_r +1;
+                    new_w = new_r +1;
                 end
                 for(int j=0; j<=4; j++) begin
                     no_w = data;
+                    counter_w = counter_r +1;
+                    new_w = new_r + 1;
                 end
 
 
@@ -30,13 +36,12 @@ endtask
 always_comb begin
     state_w         = state_r;
     count_w         = count_r;
+    counter_w         = counter_r;
+    new_w           = new_r;
     no_w         = no_r;
     case(state_r)
         S_IDLE: begin
             ff(i_data);
-            // count_w [2] = i_data;
-            // count_w [1] = i_data;
-            // count_w [0] = i_data;
             state_w = S_FINISH;
             end
 
@@ -52,12 +57,16 @@ always_ff @(posedge i_clk or posedge i_rst_n) begin
         state_r         <= S_IDLE;
         count_r         <= 0;
         no_r            <= 0;
+        counter_r       <= 0;
+        new_r           <= 0;
 
     end
     else begin
         state_r         <= state_w;
         count_r         <= count_w;
         no_r            <= no_w;
+        counter_r       <= counter_w;
+        new_r           <= new_w;
 
     end
 end
