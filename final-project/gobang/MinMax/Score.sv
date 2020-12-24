89615a60906c5eed060b0af894660e9743ec99f1
peter <xxia52122@gmail.com>
2020-12-24 19:01:51 +0800

    add score

--- final-project/gobang/MinMax/Score.sv
+++ final-project/gobang/MinMax/Score.sv
@@ -1,4 +1,4 @@
-
+// typedef logic [1:0] board [224:0];
 module Score (
 	input         i_clk,
 	input         i_rst_n,
--- final-project/gobang/tb_verilog/Score/score_tb.sv
+++ final-project/gobang/tb_verilog/Score/score_tb.sv
@@ -2,7 +2,7 @@
 
 `define CLK 10
 `define HCLK 5
-typedef logic [1:0] board [225];
+`define logic [1:0] board [225];
 
 module PG_tb;
     board i_board;
