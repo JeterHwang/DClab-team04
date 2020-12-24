ee7110231bcea1214da0ae999241a0fbedf5a0a5
jeter <jeter@example.com>
2020-12-24 14:56:25 +0800

    update

---
+++ final-project/gobang/Suansha/SearchDefense.sv
@@ -0,0 +1,12 @@
+module SearchDefense(
+    input         i_clk,
+	input         i_rst_n,
+	input         i_start,
+	input         board i_board,         // 15*15*2 bit chess board
+    input         i_turn,          
+	input         i_X,
+    input         i_Y,
+    output        hit  
+);
+    
+endmodule
