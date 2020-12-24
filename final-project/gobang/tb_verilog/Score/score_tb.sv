f892e0fbbdf824b6dfdcbda0dbf4df6e4ebb898a
peter <xxia52122@gmail.com>
2020-12-24 19:34:19 +0800

    add score

--- final-project/gobang/tb_verilog/Score/score_tb.sv
+++ final-project/gobang/tb_verilog/Score/score_tb.sv
@@ -5,7 +5,7 @@
 typedef logic [1:0] board [225];
 
 module Score_tb;
-    board i_board;
+    // board i_board;
     logic clk;
     logic start;
     logic rst_n;
@@ -19,7 +19,7 @@ module Score_tb;
         .i_clk(clk),
         .i_rst_n(rst_n),
         .i_start(start),
-        .i_board(i_board),
+        .i_board(board),
         .i_turn(1'b0),
         .o_score(score),
         .o_finish(SC_finish)
