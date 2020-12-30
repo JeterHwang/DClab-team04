5443f26a9ca110169054791f22c86fa53fd75899
hsiehjackson <fanoping@gmail.com>
2020-12-30 19:36:39 +0800

    update

--- final-project/gobang/tb_verilog/MinMax/INCA_libs/irun.lnx8664.12.20.nc/.timestamp.ts
+++ final-project/gobang/tb_verilog/MinMax/INCA_libs/irun.lnx8664.12.20.nc/.timestamp.ts
@@ -4,7 +4,7 @@
 1609257313 /home/team04/jeter/DClab-team04/final-project/gobang/MinMax/PointGen.sv
 1609316408 /home/team04/jeter/DClab-team04/final-project/gobang/MinMax/Score.sv
 1609242904 /home/team04/jeter/DClab-team04/final-project/gobang/tb_verilog/MinMax/MinmaxNode_tb.sv
-1609327819 /home/team04/jeter/DClab-team04/final-project/gobang/tb_verilog/MinMax/MinmaxWrapper_tb.sv
+1609327940 /home/team04/jeter/DClab-team04/final-project/gobang/tb_verilog/MinMax/MinmaxWrapper_tb.sv
 1609321954 /home/team04/jeter/DClab-team04/final-project/gobang/Suansha/KillWrapper.sv
 1609323281 /home/team04/jeter/DClab-team04/final-project/gobang/Suansha/KillNode.sv
 1609322318 /home/team04/jeter/DClab-team04/final-project/gobang/Suansha/Threats.sv
