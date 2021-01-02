30768d6fdb71644352bed5a2e1d81fb8f30681f0
peter <xxia52122@gmail.com>
2021-01-03 03:23:18 +0800

    update score

--- final-project/backend/model/rs232.py
+++ final-project/backend/model/rs232.py
@@ -1,13 +1,11 @@
+#!/usr/bin/env python
 from serial import Serial, EIGHTBITS, PARITY_NONE, STOPBITS_ONE
 from sys import argv
+from flask import Flask, request
+from flask import jsonify
+import time
 
-def int_to_coordinate(number):
-    tmp_string = str(number[2:])
-    while(len(tmp_string) <8):
-        tmp_string = '0'+tmp_string
-    x = int(tmp_string[0])*8+int(tmp_string[1])*4+int(tmp_string[2])*2+int(tmp_string[3])
-    y = int(tmp_string[4])*8+int(tmp_string[5])*4+int(tmp_string[6])*2+int(tmp_string[7])
-    return (x,y)
+app = Flask(__name__)
 
 assert len(argv) == 2
 s = Serial(
@@ -19,10 +17,49 @@ s = Serial(
     xonxoff=False,
     rtscts=False
 )
+def int_to_coordinate(number):
+    tmp_string = str(number[2:])
+    while(len(tmp_string) <8):
+        tmp_string = '0'+tmp_string
+    x = int(tmp_string[0])*8+int(tmp_string[1])*4+int(tmp_string[2])*2+int(tmp_string[3])
+    y = int(tmp_string[4])*8+int(tmp_string[5])*4+int(tmp_string[6])*2+int(tmp_string[7])
+    return (x,y)
+@app.route('/api', methods=['POST'])
+def postCord():
+    if request.method == "POST":
+        print(request.json["cord"])
+        cord = str(request.json["cord"])
+        print(cord)
+        s.write(int(cord).to_bytes(1,'little'))
+        print("I am a post")
+    if not request.json:
+        return "not a json post"
+    return ({"result": "all good"})
 
-for i in range(256):
-    a = i
-    s.write(a.to_bytes(1,'little'))
-    b= s.read(1)
+@app.route('/api')
+def getCord():
+    b = s.read(1)
+    whiteCord = int_to_coordinate(bin(b[0]))
     print(int_to_coordinate(bin(b[0])))
-print("your turn")
+    return str([str(whiteCord[0]), str(whiteCord[1])])
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=5000)
+
+# for i in range(10):
+#     a = i
+#     s.write(int(a).to_bytes(1,'little'))
+#     b= s.read(1)
+#     print(int_to_coordinate(bin(b[0])))
+# while(True):
+#     a = input("enter a number:")
+#     if(int(a) == 255):
+#         break
+    
+    
+#     print("your turn")
+# print("The game is over!!!")
+# def writefile(cord):
+#     f = open("demofile.txt", "w")
+#     f.write(cord)
+#     f.close()
